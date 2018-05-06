b24d0fbf2ebd16a58eb8eba343f53b1112c6b657
Chuck Bryan <cbryan@marathonus.com>
2018-05-06 16:41:41 -0400

    Intiial Commit

---
+++ StructureMap.AutoMocking.Moq/MoqAutoMocker.cs
@@ -0,0 +1,12 @@
+﻿namespace PalmettoSoft.StructureMap.AutoMocking.Moq
+{
+    using global::StructureMap.AutoMocking;
+
+    public class MoqAutoMocker<T> : AutoMocker<T> where T: class
+    {
+        public MoqAutoMocker() : base(new MoqServiceLocator())
+        {
+            ServiceLocator = new MoqServiceLocator();
+        }
+    }
+}
