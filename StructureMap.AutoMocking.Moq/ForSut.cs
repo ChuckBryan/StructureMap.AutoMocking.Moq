e8446d03a0637e583e5ffe16071360042f7d5d17
Chuck Bryan <cbryan@marathonus.com>
2018-05-06 18:02:42 -0400

    Added a simple Sut Test Base class

---
+++ StructureMap.AutoMocking.Moq/ForSut.cs
@@ -0,0 +1,32 @@
+﻿namespace StructureMap.AutoMocking.Moq
+{
+    using global::Moq;
+
+    public abstract class ForSut<TSut> where TSut : class
+    {
+        protected ForSut()
+        {
+            Setup();
+        }
+
+        protected MoqAutoMocker<TSut> Mocker { get; private set; }
+
+        protected TSut Sut => Mocker.ClassUnderTest;
+
+        private void Setup()
+        {
+            Mocker = new MoqAutoMocker<TSut>();
+
+            ConfigureIoC(Mocker.Container);
+        }
+
+        protected Mock<TInterface> GetMockFor<TInterface>() where TInterface : class
+        {
+            return Mock.Get(Mocker.Get<TInterface>());
+        }
+
+        protected virtual void ConfigureIoC(IContainer container)
+        {
+        }
+    }
+}
